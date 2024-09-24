df8c146458aba2d5750660bf034cc458a3332e1d
HarleenBhandalPXL <harleen.bhandal@student.pxl.be>
2024-09-24 11:50:48 +0200

    Sync PC

---
+++ architecture/SpringCloudPXL/notification-service/src/main/java/be/pxl/services/NotificationServiceApplication.java
@@ -0,0 +1,15 @@
+package be.pxl.services;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * NotificationServiceApplication
+ *
+ */
+public class NotificationServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
