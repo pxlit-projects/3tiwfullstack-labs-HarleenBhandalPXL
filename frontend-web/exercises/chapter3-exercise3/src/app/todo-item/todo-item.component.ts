446942d11ea695dece260e7fdace1bfa74a4ce06
HarleenBhandalPXL <harleen.bhandal@student.pxl.be>
2024-10-02 19:54:03 +0200

    Start exercises chapter 4

---
+++ frontend-web/exercises/chapter3-exercise3/src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.css'
+})
+export class TodoItemComponent {
+  title: string = "Joske";
+  checkBock : boolean = false;
+}
